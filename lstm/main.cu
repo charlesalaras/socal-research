4cbff97e533f5384d33d4b17caf73ccf705af5f8
Charles Alaras <charles.alaras@gmail.com>
2022-10-17 21:17:20 -0700

    Fail to assign memory

--- lstm/main.cu
+++ lstm/main.cu
@@ -4,8 +4,8 @@
 
 int input_size = 3; // input size
 int seqLength = 5; // timesteps
-int numLayers = 1; // num_layers
-int hiddenSize = 311; // output size
+int numLayers = 2; // num_layers
+int hiddenSize = 11; // output size
 int timeSteps = 5; // == seqLength
 int miniBatch = 5; // batch size
 //int numElements = hiddenSize * miniBatch;
@@ -58,7 +58,7 @@ int main() {
 
     cudaDeviceSynchronize();
    // Host variables
-    float * x = (float*)malloc(miniBatch * timeSteps * input_size * sizeof(float));
+    float * x = (float*)malloc((miniBatch * timeSteps * input_size + 1) * sizeof(float));
     unsigned int * lengths = (unsigned int*)malloc(miniBatch * sizeof(unsigned int));
 
     // Unified Variables
@@ -74,39 +74,47 @@ int main() {
     float * bias_ptr = NULL; // numLayers * hiddenSize * 8
     float * dropout_ptr = NULL; // input_size * input_size
 
+    float * x_ptr = NULL;
+    //unsigned int * lengths_ptr = NULL;
+
     printf("Allocating device variables\n");
     // Allocate device variables
-    cudaErrCheck(cudaMalloc(&h_data_ptr, numLayers * (seqLength + 1) * miniBatch * hiddenSize  * sizeof(float)));
-    cudaErrCheck(cudaMalloc(&c_data_ptr, numLayers * (seqLength + 1) * miniBatch * hiddenSize * sizeof(float)));
-    cudaErrCheck(cudaMalloc(&tmp_i_ptr, miniBatch * 6 * hiddenSize * sizeof(float)));
-    cudaErrCheck(cudaMalloc(&tmp_h_ptr, miniBatch * 5 * hiddenSize * sizeof(float)));
-    cudaErrCheck(cudaMalloc(&T_ptr, 6 * hiddenSize * hiddenSize * sizeof(float)));
-    cudaErrCheck(cudaMalloc(&bias_ptr, 5 * hiddenSize + 5 * hiddenSize * sizeof(float)));
+    cudaErrCheck(cudaMalloc(&h_data_ptr, (numLayers * (seqLength + 1 * miniBatch) * hiddenSize) * sizeof(float)));
+    cudaErrCheck(cudaMalloc(&c_data_ptr, (numLayers * (seqLength + 1 * miniBatch) * hiddenSize) * sizeof(float)));
+    cudaErrCheck(cudaMalloc(&tmp_i_ptr, (miniBatch * (6 * hiddenSize)) * sizeof(float)));
+    cudaErrCheck(cudaMalloc(&tmp_h_ptr, (miniBatch * (5 * hiddenSize)) * sizeof(float)));
+    // HARDCODED VALUES AHEAD
+    cudaErrCheck(cudaMalloc(&T_ptr, 2134 * sizeof(float)));
+    cudaErrCheck(cudaMalloc(&bias_ptr, 110 * sizeof(float)));
     //cudaErrCheck(cudaMalloc((void**)&dropout_ptr, numLayers * miniBatch * hiddenSize * sizeof(float)));
     // Transfer host variables to device variables
     //cudaErrCheck(cudaMemcpy(x_ptr, x, input_size * sizeof(float), cudaMemcpyHostToDevice));
     //cudaErrCheck(cudaMemcpy(lengths_ptr, lengths, input_size * input_size * sizeof(unsigned int), cudaMemcpyHostToDevice));
+    cudaErrCheck(cudaMalloc(&x_ptr, (miniBatch * timeSteps * input_size + 1) * sizeof(float)));
+    //cudaErrCheck(cudaMalloc(&lengths_ptr, miniBatch * sizeof(unsigned int)));
 
     printf("Allocating host variables");
-    float * h_data = (float*)malloc(numLayers * (seqLength + 1) * miniBatch * hiddenSize * sizeof(float));
-    float * c_data = (float*)malloc(numLayers * (seqLength + 1) * miniBatch * hiddenSize * sizeof(float));
-    float * tmp_i = (float*)malloc((miniBatch * 6 * hiddenSize * sizeof(float)));
-    float * tmp_h = (float*)malloc((miniBatch * 5 * hiddenSize * sizeof(float)));
-    float * T = (float*)malloc(6 * hiddenSize * hiddenSize * sizeof(float));
-    float * bias = (float*)malloc(5 * hiddenSize + (5 * hiddenSize) * sizeof(float));
+    float * h_data = (float*)malloc((numLayers * (seqLength + 1 * miniBatch) * hiddenSize) * sizeof(float));
+    float * c_data = (float*)malloc((numLayers * (seqLength + 1 * miniBatch) * hiddenSize) * sizeof(float));
+    float * tmp_i = (float*)malloc((miniBatch * (6 * hiddenSize) * sizeof(float)));
+    float * tmp_h = (float*)malloc((miniBatch * (5 * hiddenSize) * sizeof(float)));
+    float * T = (float*)malloc(2134 * sizeof(float));
+    float * bias = (float*)malloc(110 * sizeof(float));
     float * dropout;
     // Populate host variables
     printf("Populating variables\n");
     //populateVariables(x, lengths, h_data, c_data, tmp_i, tmp_h, T, bias, dropout);
 
     printf("Copying device variables\n");
-    cudaErrCheck(cudaMemcpy(h_data_ptr, h_data, numLayers * (seqLength + 1) * miniBatch * hiddenSize * sizeof(float), cudaMemcpyHostToDevice));
-    cudaErrCheck(cudaMemcpy(c_data_ptr, c_data, numLayers * (seqLength + 1) * miniBatch * hiddenSize * sizeof(float), cudaMemcpyHostToDevice));
-    cudaErrCheck(cudaMemcpy(tmp_i_ptr, tmp_i, miniBatch * 6 * hiddenSize * sizeof(float), cudaMemcpyHostToDevice));
-    cudaErrCheck(cudaMemcpy(tmp_h_ptr, tmp_h, miniBatch * 5 * hiddenSize * sizeof(float), cudaMemcpyHostToDevice));
-    cudaErrCheck(cudaMemcpy(T_ptr, T, 6 * hiddenSize * hiddenSize * sizeof(float), cudaMemcpyHostToDevice));
-    cudaErrCheck(cudaMemcpy(bias_ptr, bias, 5 * hiddenSize + 5 * hiddenSize * sizeof(float), cudaMemcpyHostToDevice));
+    cudaErrCheck(cudaMemcpy(h_data_ptr, h_data, (numLayers * (seqLength + 1 * miniBatch) * hiddenSize) * sizeof(float), cudaMemcpyHostToDevice));
+    cudaErrCheck(cudaMemcpy(c_data_ptr, c_data, (numLayers * (seqLength + 1 * miniBatch) * hiddenSize) * sizeof(float), cudaMemcpyHostToDevice));
+    cudaErrCheck(cudaMemcpy(tmp_i_ptr, tmp_i, miniBatch * (6 * hiddenSize) * sizeof(float), cudaMemcpyHostToDevice));
+    cudaErrCheck(cudaMemcpy(tmp_h_ptr, tmp_h, miniBatch * (5 * hiddenSize) * sizeof(float), cudaMemcpyHostToDevice));
+    cudaErrCheck(cudaMemcpy(T_ptr, T, 2134 * sizeof(float), cudaMemcpyHostToDevice));
+    cudaErrCheck(cudaMemcpy(bias_ptr, bias, 110 * sizeof(float), cudaMemcpyHostToDevice));
     //cudaErrCheck(cudaMemcpy(dropout_ptr, dropout, numLayers * miniBatch * hiddenSize * sizeof(float), cudaMemcpyHostToDevice));
+    cudaErrCheck(cudaMemcpy(x_ptr, x, (miniBatch * timeSteps * input_size + 1) * sizeof(float), cudaMemcpyHostToDevice));
+    //cudaErrCheck(cudaMemcpy(lengths_ptr, lengths, miniBatch * sizeof(float), cudaMemcpyHostToDevice));
 
     cudaDeviceSynchronize();
     printf("Running device kernel\n");
